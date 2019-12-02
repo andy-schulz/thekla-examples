6226f7a5d4a1f71c725b9c17c36049932b2b3b26
Andy Schulz <andy.schulz@test-steps.de>
2019-12-02 20:15:54 +0100

    add data tables and outline

---
+++ courses/cucumber/06_Specify_Data_in_Tables_and_DocStrings/src/__step_definition__/the_steps.js
@@ -0,0 +1,24 @@
+const {Given, Then, When} = require('cucumber');
+
+Given(`Bernhard opened a calculator`, function () {
+    console.log(`The calculater will be opend`);
+    return `success`;
+});
+
+When(`he adds the numbers`, function (table) {
+    console.log(table);
+});
+
+When(`he adds the numbers {int} and {int}`, function (int, int2) {
+    const myFirstParameter = int;
+    const mySecondParameter = int2;
+    console.log(`First Parameter ${myFirstParameter}`);
+    console.log(`Second Parameter ${mySecondParameter}`);
+
+    return `success`;
+});
+
+Then(/he can see the result (.*)/, function (int) {
+    console.log(`Expected Result: ${int}`);
+    return `success`;
+});
