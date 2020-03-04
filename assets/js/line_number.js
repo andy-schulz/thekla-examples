8057afccd4bbc1b257cc697f80acbecd98c4bd54
Andy Schulz <andy.schulz@test-steps.de>
2020-03-04 14:19:46 +0100

    fix: remove console.log from line number function

--- assets/js/line_number.js
+++ assets/js/line_number.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
         for (var j = 0; j < num-1; j++) {
             var line_num = pre[i].getElementsByTagName('span')[0];
             line_num.innerHTML += '<span>' + (j + 1) + '</span>';
-            console.log(j)
         }
     }
 })
