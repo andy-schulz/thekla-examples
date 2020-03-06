50526d1967a4d2a94c94849e6d78a0378fb057d0
Andy Schulz <andy.schulz@test-steps.de>
2020-03-06 08:35:35 +0100

    feat: include calculator.js in examples

--- courses/cucumber/060_Use_the_World_Object/cucumber.js
+++ courses/cucumber/060_Use_the_World_Object/cucumber.js
@@ -1,6 +1,6 @@
 
 const profile = {
-    "start_cmd": `px cucumber-js -r src --world-parameters {"first":2}`, // does not work jet
+    "start": `-r src --world-parameters '{"first":2, "second":4, "expected": 6}'`, // does not work jet
 };
 
 module.exports = profile;
